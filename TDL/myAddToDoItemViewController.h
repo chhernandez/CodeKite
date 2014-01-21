c02053bb7029680af401da7458ae2c1fade8f74d
chhernandez <carloshernandez@utexas.edu>
2014-01-20 23:30:27 -0600

    Add TDL

---
+++ TDL/myAddToDoItemViewController.h
@@ -0,0 +1,16 @@
+//
+//  myAddToDoItemViewController.h
+//  TDL
+//
+//  Created by Carlos Hernandez on 12/24/13.
+//  Copyright (c) 2013 Carlos Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "myToDoItem.h"
+
+@interface myAddToDoItemViewController : UIViewController
+
+@property myToDoItem *toDoItem;
+
+@end
