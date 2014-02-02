dc4c398c225d5b5e15f6e8d6d8f23317423bf2ea
chhernandez <carloshernandez@utexas.edu>
2014-02-02 13:45:36 -0600

    Added WeatherVain App to CodeKite repository
    New app created

---
+++ WeatherVain/WeatherVain/cdkViewController.h
@@ -0,0 +1,13 @@
+//
+//  cdkViewController.h
+//  WeatherVain
+//
+//  Created by Carlos Hernandez on 2/2/14.
+//  Copyright (c) 2014 Carlos Hernandez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface cdkViewController : UIViewController
+
+@end
---
+++ WeatherVain/WeatherVain/cdkViewController.m
@@ -0,0 +1,29 @@
+//
+//  cdkViewController.m
+//  WeatherVain
+//
+//  Created by Carlos Hernandez on 2/2/14.
+//  Copyright (c) 2014 Carlos Hernandez. All rights reserved.
+//
+
+#import "cdkViewController.h"
+
+@interface cdkViewController ()
+
+@end
+
+@implementation cdkViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
