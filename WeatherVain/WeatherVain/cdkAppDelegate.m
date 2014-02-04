65ecf0f43416e1ceb11e4eab9d74595c842bff44
chhernandez <carloshernandez@utexas.edu>
2014-02-03 23:21:07 -0600

    Tested splash screen delay
    Still need the right image for Weather Vain.

--- WeatherVain/WeatherVain/cdkAppDelegate.m
+++ WeatherVain/WeatherVain/cdkAppDelegate.m
@@ -12,6 +12,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    
+    sleep(15);
+    
     // Override point for customization after application launch.
     return YES;
 }
